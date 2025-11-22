95092ef37b854621c960493ae7f311ef898e8ed9
Banana Y.X. <xiaoyi9289@outlook.com>
2025-11-22 13:02:43 +0800

    手机版的标题居然藏在js里，改了

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "在线音乐-Banana Y.X.";
     }
 
     function updateMobileOverlayScrim() {
